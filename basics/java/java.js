37ae136f368653f309f9a2ce3971e02ba3f5cb6c
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-09 14:51:20 +0000

    add

---
+++ basics/java/java.js
@@ -0,0 +1,13 @@
+let my = "jieefi"
+let anothe =my
+anothe="chI"
+console.log(anothe)
+let user = {
+    email:"fkokfoef",
+    upi:"upi" 
+}
+console.log(user);
+ let use = user
+
+user.email = "jj@gmail.com"
+console.log(use);
