d7855a615fff6228ae9154bc3f761c8f7622e5d6
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-12 19:01:40 +0000

    add

---
+++ basics/java/contorleflow.js
@@ -0,0 +1,25 @@
+//if
+/*if(condition){ //(condition :boolean,variables.datatype)
+   //execution statment
+}*/
+if(5>2){
+    console.log("correct");
+}
+else{
+    console.log("incorrect");
+    
+}
+const balance = 1400
+if(balance>1000){
+    if(balance>1500){
+        console.log("congrats");
+        
+    }
+    else{
+        console.log("trynext time");
+        
+    }
+
+}
+/* switch(){
+}*/
