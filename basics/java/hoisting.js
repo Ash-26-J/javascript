9e5b2d443178c3679d8537ccd04c6d5023d8fdd5
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-12 18:05:06 +0000

    add

---
+++ basics/java/hoisting.js
@@ -0,0 +1,20 @@
+function od(){
+    const use = "hit"
+     function two(){
+       const website ="youtube"
+       console.log(use);
+     }
+     //console.log(website);
+     two()
+}
+od()
+//
+function add1(value){
+    return value+1
+}
+add1(3)
+const add2 = function(value2){
+    return value2 + 2
+}
+//add2(5)
+console.log(add2(5));
