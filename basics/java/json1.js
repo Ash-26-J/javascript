8214dcd752b0f544af053d3c8a1e49bc6b051d8c
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-12 17:07:08 +0000

    add

---
+++ basics/java/json1.js
@@ -0,0 +1,16 @@
+const course = {
+    coursename : "jjd",
+    price:"999",
+    cousreinstruct:"hhh"
+}
+const {cousreinstruct:inst} = course
+console.log(inst);
+//destructuring 
+const navbar = ({company}) =>{
+
+}
+navbar(company = "jj")
+console.log(company);
+
+//api 
+//api is json format (json - javascript object notation)
