9882d4c4947bebc68b323a84900b0f286ca70b9c
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-12 19:22:19 +0000

    add

---
+++ basics/java/for.js
@@ -0,0 +1,48 @@
+for (let index = 0; index < 10; index++) {
+    const element = index
+    console.log(element);
+    
+}
+console.log("tables");
+for (let index = 1; index < 10; index++) {
+    for (let index2 = 1; index2 < 10; index2++){
+       
+        
+      console.log(index+'*'+index2+'='+index*index2);
+      
+1       
+    }
+    
+}
+
+let arr =[100,20,300,66,44]
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+    
+    
+}
+let arr1 =["john","hello","jack","james","jane"]
+for (let index = 0; index < arr1.length; index++) {
+    const element = arr1[index];
+    console.log(element);
+    
+    
+}
+// key words
+//break and continue
+for (let index = 0; index < 20; index++) {
+   if(index == 18 ){
+    console.log(index);
+   break
+}
+console.log(index);
+}
+
+for (let index = 0; index < 20; index++) {
+    if(index == 18 ){
+     console.log(index);
+    continue
+ }
+ console.log(index);
+ }
