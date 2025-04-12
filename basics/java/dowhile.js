f6174ad0f9560e8d0f763467582d707aa55f18df
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-12 19:48:41 +0000

    add

---
+++ basics/java/dowhile.js
@@ -0,0 +1,19 @@
+let arr =[1,2,34,4,5]
+for (const i of arr) {
+    console.log(i);
+    
+    
+}
+const greet = "geeed"
+for (const greetings of greet) {
+  console.log('sss'+greetings);
+     
+  //map
+  const map = new Map()
+  map.set('in',"india");
+  map.set('state',"karnatak");
+  map.set('set',"set1");
+  map.set('setr',"setty");
+
+  console.log(map);
+  
---
+++ basics/java/while.js
@@ -0,0 +1,20 @@
+/*while(condition)
+{
+    //condition
+}*/
+
+/*let i =0
+while (i<10) {
+    console.log("value of index"+i);
+    i = i+2;
+}*/
+
+let i =0
+let arr =["jack","john","james","jefrry","jenny"]
+while (i<arr.length) {
+    console.log('value is'+arr[i]);
+    
+    i=i+1
+    
+}
+
