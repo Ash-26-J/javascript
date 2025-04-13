fb0cdd738bbf38d3dfc5e51cabdaee85e061f2d0
Ashish Joseph <joseph.2022grwd@gmail.com>
2025-04-13 19:18:27 +0000

    add

--- basics/java/map.js
+++ basics/java/map.js
@@ -16,3 +16,46 @@ const fradric =fred.filter((num)=> {
    return  num >22
 })
 console.log(fradric);
+//if
+
+const frbd = [11,22,3,66,55,66,44];
+const frbdric =frbd.filter((num)=> {
+    if(num > 4){
+        frbd.push(num)
+    }
+   return  num >22
+})
+console.log(frbdric);
+//
+const books =[
+    {
+        title: 'dbms', publisher: "oswal",publish:2001
+    },
+    {
+        title: 'os', publisher: "mcgrawhil",publish:1889
+    },
+    {
+        title: 'js', publisher: "mc grawhill",publish:2009
+    },
+    {
+        title: 'npm', publisher: "hc hills",publish:2001
+    },
+    {
+        title: 'cloud', publisher: "hc hills",publish:2001
+    },
+]
+const book =books.filter((bk)=>{
+    if(bk.publish === 2001)
+    {
+        return bk.publish 
+    }
+})
+console.log(book);
+//similarly
+const booko =books.filter((bk)=>{
+    if(bk.title === 'os')
+    {
+        return bk.title
+    }
+})
+console.log(booko);
